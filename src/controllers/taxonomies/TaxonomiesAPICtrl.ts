d3a3193e67e1f85230fed63b19ca25bcc9e8422b
joshuanwafor <joshuanwafor01@gmail.com>
2021-11-18 15:41:57 +0100

    ok

---
+++ src/controllers/taxonomies/TaxonomiesAPICtrl.ts
@@ -0,0 +1,41 @@
+import { BodyParams, Controller, Delete, Get, PathParams, Post, Put, UseAuth } from "@tsed/common";
+import { TaxonomyService } from "../../services/taxonomies/TaxonomyService";
+import { AuthService } from "../../services/auth";
+
+@Controller({
+  path: "/taxonomies",
+})
+
+export class TaxonomiesCtrl {
+  constructor(private service: TaxonomyService, private authService: AuthService) {
+
+  }
+
+  @Get("/")
+  async getItems() {
+    return this.service.query({ creator_id: this.authService.user_id });
+  }
+
+  @Post("")
+  async postItem(@BodyParams() body: any) {
+    body.creator_id = this.authService.user_id;
+    return await this.service.save(body)
+  }
+
+  @Put("/:itemID")
+  async updateItem(@BodyParams() body: any, @PathParams("itemID") itemID: string) {
+    body._id = itemID;
+    try {
+      await this.service.save(body);
+      return true;
+    } catch (e) {
+
+    }
+  }
+
+  @Delete("/:itemID")
+  async deleteItem(@PathParams("itemID") itemID: string) {
+    return await this.service.remove(itemID)
+  }
+
+}
--- src/public-controllers/index-ctrl.ts
+++ src/public-controllers/index-ctrl.ts
@@ -12,20 +12,63 @@ import {
 import { MergeParams } from "@tsed/platform-express";
 import { Required, Status, Description, Summary } from "@tsed/schema";
 import { NotFound } from "@tsed/exceptions";
+import { TaxonomyService } from "../services/taxonomies/TaxonomyService";
+import { ProjectService } from "../services/projects/ProjectService";
 
 @Controller("/")
 
 export class EventsCtrl {
-
+    constructor(private tcontroler: TaxonomyService, private pcontroler: ProjectService,) { }
     @View("index.ejs")
     @Get("/")
-    get() {
-
+    async get() {
+        let resources = await this.getPageResouces();
+        return { ...resources }
     }
 
     @View("taxonomies.ejs")
     @Get("/taxonomies")
-    getCategories() {
+    async getCategories() {
+        let resources = await this.getPageResouces();
+        return { ...resources }
+    }
 
+
+    async getPageResouces(): Promise<{ taxonomies: any }> {
+        let items = await this.tcontroler.query({});
+        return {
+            taxonomies: items
+        }
+    }
+
+
+    @View("projects.ejs")
+    @Get("/projects")
+    async getProjects() {
+        let projects = await this.pcontroler.query({});
+        let resources = await this.getPageResouces();
+        return { ...resources, projects: projects }
+    }
+
+    @View("project.ejs")
+    @Get("/projects/:itemID")
+    async getProject(@PathParams("itemID") itemID: string) {
+        let item = await this.pcontroler.find(itemID);
+        let resources = await this.getPageResouces();
+        return {
+            ...resources,
+            project: item,
+        };
+    }
+
+    @View("index.ejs")
+    @Get("/projects/:itemID/purchase")
+    async purchaseProject(@PathParams("itemID") itemID: string) {
+        let item = await this.pcontroler.find(itemID);
+        let resources = await this.getPageResouces();
+        return {
+            ...resources,
+            project: item,
+        };
     }
 }
