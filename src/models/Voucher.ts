2f5c0b00c7ff52d2c786761b92a20caf536cadb3
joshuanwafor <joshuanwafor01@gmail.com>
2021-12-22 11:38:16 +0100

    ok

--- src/models/Voucher.ts
+++ src/models/Voucher.ts
@@ -1,4 +1,4 @@
-import { Required, Name, Description, Property } from "@tsed/schema";
+import { Required, Name, Description, Property, Schema } from "@tsed/schema";
 
 import { Model, Ref } from "@tsed/mongoose";
 
@@ -13,6 +13,46 @@ export class VoucherModel {
   @Property()
   @Description("")
   publisher_id: string;
+
+  @Name("created_at")
+  @Property()
+  createdAt: any;
+
+  @Name("updated_at")
+  @Property()
+  updatedAt: any;
+
+  @Property()
+  @Description("")
+  status: string;
+
+  @Property()
+  @Description("")
+  note: string;
+
+  @Property()
+  repayment_date: string;
+  @Property()
+  interest_rate: string;
+
+  loan_response: VoucherLenderResponse;
+}
+
+@Schema({})
+class VoucherLenderResponse {
+  @Property()
+  lender_id: string;
+
+  @Property()
+  status: string;
+
+  @Property()
+  note: string;
+
+  @Property()
+  repayment_date: string;
+  @Property()
+  interest_rate: string;
 
   @Name("created_at")
   @Property()
